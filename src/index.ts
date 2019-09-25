a6b16e4cd0d740423e0996b4f0796af7cd08eb31
CodeHz <CodeHz@Outlook.com>
2019-09-25 14:13:58 +0800

    basic architect

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,57 @@
 import * as io from 'packedio'
-import { ChatPacket, buildPacket, parsePacket, TextPacket, TextPacketPlainTextPayload, DisconnectPacket } from './packets';
+import {
+  ChatPacket,
+  buildPacket,
+  parsePacket,
+  TextPacket,
+  TextPacketPlainTextPayload,
+  DisconnectPacket,
+  GameStartPacket,
+} from './packets'
 
 async function main() {
-  const ticket = await getTicket("http://127.0.0.1:1984", "default", "codehz")
-  start("ws://127.0.0.1:2480", ticket)
+  const ticket = await getTicket('http://127.0.0.1:1984', 'default', 'codehz')
+  start('ws://127.0.0.1:2480', ticket)
 }
 
-async function getTicket(endpoint: string, serverid: string, username: string): Promise<ArrayBuffer> {
-  const req = await fetch(`${endpoint}/login/${serverid}/${username}`, { mode: 'cors' });
+async function getTicket(
+  endpoint: string,
+  serverid: string,
+  username: string,
+): Promise<ArrayBuffer> {
+  const req = await fetch(`${endpoint}/login/${serverid}/${username}`, {
+    mode: 'cors',
+  })
   return req.arrayBuffer()
 }
 
-var conn: WebSocket
+enum Stage {
+  Closed = -1,
+  Initial = 0,
+  Running = 1,
+}
+
+let conn: WebSocket
+let stage: Stage = Stage.Closed
 
 function start(address: string, ticket: ArrayBuffer) {
   conn = new WebSocket(address)
-  conn.binaryType = "arraybuffer"
-  conn.addEventListener("open", () => further(ticket))
-  conn.addEventListener("message", ev => handle(ev.data as ArrayBuffer))
+  conn.binaryType = 'arraybuffer'
+  conn.addEventListener('open', () => further(ticket))
+  conn.addEventListener('message', ev => handle(ev.data as ArrayBuffer))
+  conn.addEventListener('close', () => stage = Stage.Closed)
 }
 
 function further(ticket: ArrayBuffer) {
   conn.send(ticket)
+  stage = Stage.Initial
 
-  const input = document.createElement("input")
+  const input = document.createElement('input')
   document.body.append(input)
-  input.addEventListener("keyup", ev => {
+  input.addEventListener('keyup', ev => {
     if (ev.keyCode == 13) {
       const pkt = createChatPacket(input.value)
-      input.value = ""
+      input.value = ''
       conn.send(pkt)
     }
   })
@@ -37,19 +60,36 @@ function further(ticket: ArrayBuffer) {
 function handle(buffer: ArrayBuffer) {
   const i = new io.Input(new DataView(buffer))
   const pkt = parsePacket(i)
-  console.log(pkt)
-  if (pkt instanceof TextPacket) {
-    if (pkt.payload instanceof TextPacketPlainTextPayload) {
-      console.log(pkt.sender + ":" + pkt.payload.content)
-    }
-  } else if (pkt instanceof DisconnectPacket) {
-    console.warn(pkt.message)
+  console.log('Received packet %o', pkt)
+  switch (stage) {
+    case Stage.Initial:
+      if (pkt instanceof GameStartPacket) {
+        stage = Stage.Running
+      } else if (pkt instanceof DisconnectPacket) {
+        console.warn("Disconnected ", pkt.message)
+        stage = Stage.Closed
+      } else {
+        console.error('Unexcepted packet %o', pkt)
+      }
+      break
+    case Stage.Running:
+      if (pkt instanceof TextPacket) {
+        if (pkt.payload instanceof TextPacketPlainTextPayload) {
+          console.log(pkt.sender + ':' + pkt.payload.content)
+        }
+      } else if (pkt instanceof DisconnectPacket) {
+        console.warn("Disconnected %o", pkt.message)
+        stage = Stage.Closed
+      }
+      break
+    default:
+      console.error('Unimplemented stage %d', stage)
   }
 }
 
 function createChatPacket(text: string): ArrayBuffer {
   const out = new io.Output()
-  const pkt = new ChatPacket
+  const pkt = new ChatPacket()
   pkt.message = text
   buildPacket(out, pkt)
   return out.write()
